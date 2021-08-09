7dfffc32d492f810be62645f9eb5b1c47d4569cf
dzlandis <49926644+dzlandis@users.noreply.github.com>
2021-08-09 00:44:18 -0700

    hehe cleaning

--- commands/ping.js
+++ commands/ping.js
@@ -4,12 +4,10 @@ module.exports = {
   name: 'ping',
   description: "Pings the bot and shows API Latency.",
   async execute(interaction) {
-    // Have the client start typing:
-    // Send back a reply when the specific command has been written
       const ping = new Date().getTime() - interaction.createdTimestamp;
       const APILatency = interaction.client.ws.ping
       // npm i @lukeed/ms (basically a hyperfast ms lib)
-      const { format } = require('@lukeed/ms'); // Credit to Axis#1010 for showing me!
+      const { format } = require('@lukeed/ms');
       const uptime = format(interaction.client.uptime, true); // 1m (if uptime === 60000)
 
       const pingEmbed = new Discord.MessageEmbed()
